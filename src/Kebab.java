740b5f59a7c6c69fea53b47d48f55a58170e30cb
Mehmettarhan <m.sirintarhan212@gmail.com>
2021-07-21 15:26:48 -0500

    opened a for loop

---
+++ src/Kebab.java
@@ -0,0 +1,12 @@
+public class Kebab {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Zahra");
+        for (int i = 0; i <= 21 ; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
